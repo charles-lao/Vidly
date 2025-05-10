043026ff0b17e26e5f0f77b404737693ebc1634c
charles-lao <charles.h.lao@gmail.com>
2025-05-10 13:54:25 +1200

    Complete Lesson 51 - Adding Validation

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
+using Vidly.Migrations;
 using Vidly.Models;
 using Vidly.ViewModels;
 
@@ -35,6 +36,15 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if(!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -17,8 +17,8 @@ namespace Vidly.Models
         public bool IsSubscribedToNewsletter { get; set; }
        
         public MembershipType MembershipType { get; set; }
-        [Display(Name = "Membership Type")]
         
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
         
         [Display(Name = "Date of Birth")]
